a55e48f44c5edbd408a8968059c65455f885c457
DeraJSP <derajsp@gmail.com>
2024-06-18 09:18:38 +0100

    01_day

---
+++ 01_Day/datatypes.js
@@ -0,0 +1,36 @@
+console.log(
+	typeof stringVar,
+	typeof booleanVar,
+	typeof nullVar,
+	typeof undefinedVAr,
+);
+
+let gadgets;
+let socks;
+let pen;
+let equipments;
+
+const cook = "Walter White";
+const car = "Aztec";
+const product = "Blue Sky";
+const isJesseJames = false;
+
+const firstName = "Gus";
+const lastName = "Fring";
+const maritalStatus = "Single";
+const country = "Chile";
+const age = 51;
+
+/*
+	const firstName = "Gus",
+		lastName = "Fring",
+		maritalStatus = "Single",
+		country = "Chile",
+		age = 51;
+*/
+
+const myAge = 31;
+const yourAge = 23;
+
+console.log(`I am ${myAge} years old.
+You are ${yourAge} years old.`);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 30DaysOfJS
+This is the 30 days of JavaScript coding challenge by Asabeheh
